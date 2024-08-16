3efe9cba19e7d53afa4794c01bf0bc424c228382
jackson van duikeren <jackovand27@icloud.com>
2024-08-16 15:10:02 +1000

    Added basic constructor with itemid, itemname, weight

--- source/core/src/main/com/csse3200/game/components/items/ItemComponent.java
+++ source/core/src/main/com/csse3200/game/components/items/ItemComponent.java
@@ -1,4 +1,16 @@
 package com.csse3200.game.components.items;
 
-public class ItemComponent {
+import com.csse3200.game.components.Component;
+
+public class ItemComponent extends Component {
+    private final String itemid;
+    private final String itemName;
+    private final int weight;
+    private boolean isInteractable;
+
+    public ItemComponent(String itemid, String itemName, int weight) {
+        this.itemid = itemid;
+        this.itemName = itemName;
+        this.weight = weight;
+    }
 }
