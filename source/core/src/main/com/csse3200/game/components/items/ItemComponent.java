1c425b079b6c4dd38316ec5ca2f585a655247685
Jason Mauchline <j.mauchline@uqconnect.edu.au>
2024-08-17 20:16:19 +1000

    Created getters for Item Component class

--- source/core/src/main/com/csse3200/game/components/items/ItemComponent.java
+++ source/core/src/main/com/csse3200/game/components/items/ItemComponent.java
@@ -5,12 +5,13 @@ public class ItemComponent extends Component {
     private final String itemid;
     private final String itemName;
     private final int weight;
-    private boolean isInteractable;
+    private final String itemType;
 
-    public ItemComponent(String itemid, String itemName, int weight) {
+    public ItemComponent(String itemid, String itemName, String itemType, int weight) {
         this.itemid = itemid;
         this.itemName = itemName;
         this.weight = weight;
+        this.itemType = itemType;
     }
 
     public String getItemid() {
@@ -20,4 +21,14 @@ public class ItemComponent extends Component {
     public String getItemName() {
         return this.itemName;
     }
+
+    public int weight() {
+        return this.weight;
+    }
+
+    public String getItemType() {
+        return this.itemType;
+    }
+
+
 }
