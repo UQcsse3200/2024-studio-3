c55d03fd3308e9669a4178010c5f453713b690ce
aridspace10 <jackovand27@icloud.com>
2024-08-19 22:08:43 +1000

    Added basic 3 types (raw fish, beef, chicken)

--- source/core/src/main/com/csse3200/game/components/items/ItemType.java
+++ source/core/src/main/com/csse3200/game/components/items/ItemType.java
@@ -1,17 +1,8 @@
 package com.csse3200.game.components.items;
 
-public class ItemType {
+public enum ItemType {
+    RAWFISH,
+    BEEF,
+    CHICKEN
 
-    public enum ItemList {
-
-        White_Bread,
-        Grain_Bread,
-        Chicken,
-        Beef,
-        Lettuce,
-        Tomato,
-        Cheese //Current food items
-
-
-    }
 }
