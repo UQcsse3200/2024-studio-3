741dc10baaac93885f90b1fff0126b582b09abfc
ashom2 <alekid804@gmail.com>
2024-08-20 13:03:34 +1000

    Added Create, Update, addItem and removeItem methods.

--- source/core/src/main/com/csse3200/game/components/station/CookingComponent.java
+++ source/core/src/main/com/csse3200/game/components/station/CookingComponent.java
@@ -7,4 +7,34 @@ import com.csse3200.game.components.Component;
  */
 public class CookingComponent extends Component {
 
+    /**
+     * Called on creation of the station and adds listeners.
+     */
+    @Override
+    public void create() {
+        entity.getEvents().addListener("Add Station Item", this::addItem);
+        entity.getEvents().addListener("Remove Station Item", this::removeItem);
+    }
+
+    /**
+     * Called every frame to incrementally cook items.
+     */
+    @Override
+    public void update() {
+        // Add to cooking timer and cook item
+    }
+
+    /**
+     * Method triggered when item added to station.
+     */
+    private void addItem() {
+
+    }
+
+    /**
+     * Method triggered when item removed from station.
+     */
+    private void removeItem() {
+
+    }
 }
