ff1910cf399c0b36f35e1e4d8eca310f19f00cb9
ashom2 <alekid804@gmail.com>
2024-08-20 12:40:20 +1000

    Created CookingComponent class.

---
+++ source/core/src/main/com/csse3200/game/components/station/CookingComponent.java
@@ -0,0 +1,10 @@
+package com.csse3200.game.components.station;
+
+import com.csse3200.game.components.Component;
+
+/**
+ * A component used to handle changing the state of an item being passed through a station.
+ */
+public class CookingComponent extends Component {
+
+}
