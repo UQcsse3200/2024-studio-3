e84af1ae1fe0ff38037cd986cb3e56364c9c68ba
daleelakhairil <d.khairil@uq.edu.au>
2024-08-22 13:37:08 +1000

    parsing json file

--- source/core/src/main/com/csse3200/game/entities/factories/CookingFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/CookingFactory.java
@@ -1,29 +1,16 @@
-/*
+
 package com.csse3200.game.entity.factory;
 
 import com.csse3200.game.entities.config.CookingConfig;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.csse3200.game.entities.configs.CookingConfig;
+import com.csse3200.game.entities.configs.NPCConfigs;
+import com.csse3200.game.files.FileLoader;
+
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
-
 public class CookingFactory {
 
-    public static List<CookingConfig> initializeRecipes(String jsonFilePath) {
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        try {
-            // Parse JSON file into a list of CookingConfig objects
-            CookingConfig[] recipeArray = objectMapper.readValue(new File(jsonFilePath), CookingConfig[].class);
-            for (CookingConfig recipe : recipeArray) {
-                recipes.add(recipe);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return recipes;
-    }
-
-}*/
+    private static final CookingConfig configs =
+            FileLoader.readClass(CookingConfig.class, "configs/recipe.json");
+}
