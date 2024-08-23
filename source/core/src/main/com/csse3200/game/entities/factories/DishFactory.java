6446016acd52ed0a18cbd19a046c79b93a4a9be8
Joanna Wang <s4740105@student.uq.edu.au>
2024-08-23 16:39:59 +1000

    move the docstring of the DishFactory.java before the class

--- source/core/src/main/com/csse3200/game/entities/factories/DishFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/DishFactory.java
@@ -5,14 +5,13 @@ package com.csse3200.game.entities.factories;
 import com.csse3200.game.entities.configs.CookingConfig;
 import com.csse3200.game.files.FileLoader;
 
+/**
+ * Factory to create a cooking entity with predefined components.
+ *
+ * <p>Predefined recipe properties are loaded from a config stored as a json file and should have
+ * the properties stores in 'CookingConfig'.
+ */
 public class DishFactory {
-
-    /**
-     * Factory to create recipe entities with predefined components.
-     *
-     * <p>Predefined recipe properties are loaded from a config stored as a json file and should have
-     * the properties stores in 'CookingConfig'.
-     */
     private static final CookingConfig configs =
             FileLoader.readClass(CookingConfig.class, "configs/recipe.json");
 }
