407f75fe4834ab1e06cd0017dcad867c3628eeca
Michael Kidd <4michaelkidd@gmail.com>
2024-08-24 12:59:18 +1000

    Created new class Docket, for purposes of modularity and future systems. Docket will include information such as what the recipe is, what the food is, how much time the customer has, who the customer is and textures

---
+++ source/core/src/main/com/csse3200/game/components/ordersystem/Docket.java
@@ -0,0 +1,11 @@
+package com.csse3200.game.components.ordersystem;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.csse3200.game.ui.UIComponent;
+
+public class Docket extends UIComponent {
+    @Override
+    protected void draw(SpriteBatch batch) {
+        //Do not need to do anything here :)
+    }
+}
