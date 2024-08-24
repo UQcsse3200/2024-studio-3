fd92663d463a4488b803182818c21dadce24a53a
Michael Kidd <4michaelkidd@gmail.com>
2024-08-24 17:52:19 +1000

    Added code to DocketService class, including a constructor method and a method called getEvents(), which returns the eventGandler private variable

--- source/core/src/main/com/csse3200/game/services/DocketService.java
+++ source/core/src/main/com/csse3200/game/services/DocketService.java
@@ -1,4 +1,15 @@
 package com.csse3200.game.services;
 
+import com.csse3200.game.events.EventHandler;
+
 public class DocketService {
+    private final EventHandler docketEventHandler;
+
+    public DocketService() {
+        docketEventHandler = new EventHandler();
+    }
+
+    public EventHandler getEvents() {
+        return docketEventHandler;
+    }
 }
