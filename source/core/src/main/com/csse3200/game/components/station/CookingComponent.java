60c4c439836406849b569eb1d51fb66ac0e2a104
ashom2 <alekid804@gmail.com>
2024-08-24 14:16:43 +1000

    Added timer functionality.

--- source/core/src/main/com/csse3200/game/components/station/CookingComponent.java
+++ source/core/src/main/com/csse3200/game/components/station/CookingComponent.java
@@ -1,17 +1,23 @@
 package com.csse3200.game.components.station;
 
 import com.csse3200.game.components.Component;
+import com.csse3200.game.services.GameTime;
 
 /**
  * A component used to handle changing the state of an item being passed through a station.
  */
 public class CookingComponent extends Component {
+    private StationInventoryComponent inventoryComponent;
+    private GameTime gameTime;
+    private long cookingTime;
+    private boolean isCooking;
 
     /**
      * Called on creation of the station and adds listeners.
      */
     @Override
     public void create() {
+        inventoryComponent = entity.getComponent(StationInventoryComponent.class);
         entity.getEvents().addListener("Add Station Item", this::addItem);
         entity.getEvents().addListener("Remove Station Item", this::removeItem);
     }
@@ -22,19 +28,25 @@ public class CookingComponent extends Component {
     @Override
     public void update() {
         // Add to cooking timer and cook item
+        if (inventoryComponent.isItemPresent() && isCooking) { // TODO might be redundant
+            // TODO add method to check if overcooked
+            cookingTime += gameTime.getDeltaTime();
+        }
     }
 
     /**
      * Method triggered when item added to station.
      */
     private void addItem() {
-
+        // TODO check valid item first - can store invalid but can't cook it
+        cookingTime = 0;
+        isCooking = true;
     }
 
     /**
      * Method triggered when item removed from station.
      */
     private void removeItem() {
-
+        isCooking = false;
     }
 }
