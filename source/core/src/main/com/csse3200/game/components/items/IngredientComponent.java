1980c919c66e8a4f780c1642a83115243898b0b8
Jordan Russo <jordan.russo@student.uq.edu.au>
2024-08-24 23:34:19 +1000

    Changed itemState to String representation

--- source/core/src/main/com/csse3200/game/components/items/IngredientComponent.java
+++ source/core/src/main/com/csse3200/game/components/items/IngredientComponent.java
@@ -2,18 +2,12 @@ package com.csse3200.game.components.items;
 
 public class IngredientComponent extends ItemComponent{
         private final int cookTime;
-        private ItemState itemState;
-        private enum ItemState {
-            RAW,
-            COOKED,
-            BURNED,
-            PREPARED
-        }
+        private String itemState;
 
-    public IngredientComponent(String itemName, ItemType itemType, int weight, int cookTime) {
+    public IngredientComponent(String itemName, ItemType itemType, int weight, int cookTime, String itemState) {
         super(itemName, itemType, weight);
         this.cookTime = cookTime;
-        this.itemState = ItemState.RAW;
+        this.itemState = itemState;
     }
 
     public int getCookTime() {
@@ -21,14 +15,14 @@ public class IngredientComponent extends ItemComponent{
     }
 
     public String getItemState() {
-        return this.itemState.name();
+        return this.itemState;
     }
 
     public void cookItem() {
-        this.itemState = ItemState.COOKED;
+        this.itemState = "cooked";
     }
 
     public void burnItem() {
-        this.itemState = ItemState.BURNED;
+        this.itemState = "burnt";
     }
 }
