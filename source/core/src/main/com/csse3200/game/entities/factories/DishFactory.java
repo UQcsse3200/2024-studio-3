96d6da55db91e84c111fe1614d3dcc73dab471ae
Joanna Wang <s4740105@student.uq.edu.au>
2024-08-25 18:01:39 +1000

    add function for getting recipe given the ingredients

--- source/core/src/main/com/csse3200/game/entities/factories/DishFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/DishFactory.java
@@ -3,10 +3,16 @@ package com.csse3200.game.entities.factories;
 
 
 import com.csse3200.game.entities.configs.CookingConfig;
+import com.csse3200.game.entities.configs.SingleStationRecipeConfig;
+import com.csse3200.game.entities.configs.MultiStationRecipeConfig;
+
+
 import com.csse3200.game.files.FileLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -20,4 +26,45 @@ public class DishFactory {
     private static final CookingConfig configs =
             FileLoader.readClass(CookingConfig.class, "configs/recipe.json");
 
+    private static Map<String, SingleStationRecipeConfig> getSingleStationRecipes() {
+        Map<String, SingleStationRecipeConfig> singleStationRecipes = new HashMap<>();
+        singleStationRecipes.put("acaiBowl", configs.acaiBowl);
+        singleStationRecipes.put("salad", configs.salad);
+        singleStationRecipes.put("fruitSalad", configs.fruitSalad);
+        singleStationRecipes.put("bananaSplit", configs.bananaSplit);
+        return singleStationRecipes;
+    }
+
+    private static Map<String, MultiStationRecipeConfig> getMultiStationRecipes() {
+        Map<String, MultiStationRecipeConfig> multiStationRecipes = new HashMap<>();
+        multiStationRecipes.put("steakMeal", configs.steakMeal);
+        return multiStationRecipes;
+    }
+
+    public static List<String> getRecipe (String ingredient) {
+        List<String> recipes = new ArrayList<>();
+
+        if (configs == null) {
+            return recipes;
+        }
+
+        for (Map.Entry<String, SingleStationRecipeConfig> entry : getSingleStationRecipes().entrySet()) {
+            String recipe = entry.getKey();
+            SingleStationRecipeConfig recipeConfig = entry.getValue();
+
+            if (recipeConfig.ingredient.contains(ingredient)) {
+                recipes.add(recipe);
+            }
+        }
+
+        for (Map.Entry<String, MultiStationRecipeConfig> entry : getMultiStationRecipes().entrySet()) {
+            String recipe = entry.getKey();
+            MultiStationRecipeConfig recipeConfig = entry.getValue();
+
+            if (recipeConfig.ingredient.contains(ingredient)) {
+                recipes.add(recipe);
+            }
+        }
+        return recipes;
+    }
 }
