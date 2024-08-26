e4934651cb259f8401e8ed2455872b3eb88c4dcc
Alex <alex.ong@uqconnect.edu.au>
2024-08-26 17:28:47 +1000

    fixed map error, re-added bench file and  createBench functions

---
+++ source/core/src/main/com/csse3200/game/entities/Benches/Bench.java
@@ -0,0 +1,70 @@
+package com.csse3200.game.entities.Benches;
+
+import java.util.ArrayList;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.csse3200.game.components.station.StationItemHandlerComponent;
+import com.csse3200.game.entities.Entity;
+import com.csse3200.game.physics.PhysicsLayer;
+import com.csse3200.game.physics.PhysicsUtils;
+import com.csse3200.game.physics.components.ColliderComponent;
+import com.csse3200.game.physics.components.PhysicsComponent;
+import com.csse3200.game.rendering.TextureRenderComponent;
+
+public class Bench {
+/**
+ * Creates visible station.
+ * @param height Station height in world units
+ * @param type Type of station
+ * @return Station entity of given width and height with relavent behaviors
+ */
+    /**
+     * Creates visible station.
+     *
+     * @param height Station height in world units
+     * @param type   Type of station
+     * @return Station entity of given width and height with relavent behaviors
+     */
+    public static Entity createBench(String type, float height) {
+        Entity bench = new Entity()
+                .addComponent(new TextureRenderComponent("images/stations/" + type + ".png"))
+                .addComponent(new PhysicsComponent())
+                .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE))
+                .addComponent(new StationItemHandlerComponent(type, new ArrayList<>()));
+
+
+        bench.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
+        bench.getComponent(TextureRenderComponent.class).scaleEntity();
+        bench.scaleHeight(height);
+
+        PhysicsUtils.setScaledCollider(bench, 1.2F, 0.8F);
+
+        return bench;
+    }
+
+    /**
+     * Creates visible station.
+     *
+     * @param height Station height in world units
+     * @param type   Type of station
+     * @param xScale Bench collision in x
+     * @param yScale Bench collision in y
+     * @return Station entity of given width and height with relavent behaviors
+     */
+    public static Entity createBench(String type, float height, float xScale, float yScale) {
+        Entity bench = new Entity()
+                .addComponent(new TextureRenderComponent("images/stations/" + type + ".png"))
+                .addComponent(new PhysicsComponent())
+                .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE))
+                .addComponent(new StationItemHandlerComponent(type, new ArrayList<>()));
+
+
+        bench.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
+        bench.getComponent(TextureRenderComponent.class).scaleEntity();
+        bench.scaleHeight(height);
+
+        PhysicsUtils.setScaledCollider(bench, xScale, yScale);
+
+        return bench;
+    }
+
+}
