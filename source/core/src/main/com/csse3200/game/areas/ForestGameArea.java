a042f3a22c939792940547e3744e5a3424291a55
Jordan Russo <jordan.russo@student.uq.edu.au>
2024-08-26 17:43:50 +1000

    Removed current test spawning from example game

--- source/core/src/main/com/csse3200/game/areas/ForestGameArea.java
+++ source/core/src/main/com/csse3200/game/areas/ForestGameArea.java
@@ -77,10 +77,7 @@ public class ForestGameArea extends GameArea {
     spawnTerrain();
     //spawnTrees();
     player = spawnPlayer();
-    spawnFish("raw");
-    spawnBeef("cooked");
-    spawnBanana("chopped");
-    spawnFruitSalad();
+
     spawnGhosts();
     spawnGhostKing();
 
