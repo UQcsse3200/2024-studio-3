b152656e16b0fbb714584413fbe803558d44c274
Caleb Murphy <calebmurphy2003@gmail.com>
2024-08-26 18:12:36 +1000

    #11 testing added for interaction component

--- source/core/src/main/com/csse3200/game/physics/components/InteractionComponent.java
+++ source/core/src/main/com/csse3200/game/physics/components/InteractionComponent.java
@@ -5,9 +5,21 @@ package com.csse3200.game.physics.components;
  * A physics component that detects a collision with an interactable object
  */
 public class InteractionComponent extends ColliderComponent {
+
+    private short targetLayer;
+
+    public InteractionComponent(short targetLayer) {
+        super();
+        this.targetLayer = targetLayer;
+    }
+
     @Override
     public void create() {
         setSensor(true);
         super.create();
+    }
+
+    public short getTargetLayer() {
+        return this.targetLayer;
     }
 }
