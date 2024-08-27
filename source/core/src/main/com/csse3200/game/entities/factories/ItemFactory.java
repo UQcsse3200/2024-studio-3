c9bca420a245793dc9f55f5cae9ff23a64b5748b
Madeleine Coombs <madeleinecoombs@outlook.com>
2024-08-27 11:30:37 +1000

    Fixes IngredientComponent logic to handle items being choppable, cookable, both or neither. Adds appropriate getters. Related to #67.

--- source/core/src/main/com/csse3200/game/entities/factories/ItemFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/ItemFactory.java
@@ -30,7 +30,8 @@ public class ItemFactory {
      */
     public static Entity createFish(String cookedLevel) {
         Entity fish = createTemplateItem()
-                .addComponent(new IngredientComponent("Fish", ItemType.FISH, 2, 10, cookedLevel))
+                .addComponent(new IngredientComponent("Fish", ItemType.FISH, 2, 10,
+                        0, cookedLevel))
                 .addComponent(new TextureRenderComponent(String.format("images/%s_fish.png", cookedLevel)));
         PhysicsUtils.setScaledCollider(fish, 0.6f, 0.3f);
         fish.getComponent(ColliderComponent.class).setDensity(1.5f);
@@ -45,7 +46,8 @@ public class ItemFactory {
      */
     public static Entity createBeef(String cookedLevel) {
         Entity beef = createTemplateItem()
-                .addComponent(new IngredientComponent("Beef", ItemType.BEEF, 2, 10, cookedLevel))
+                .addComponent(new IngredientComponent("Beef", ItemType.BEEF, 2, 10,
+                        0, cookedLevel))
                 .addComponent(new TextureRenderComponent(String.format("images/%s_beef.png", cookedLevel)));
         PhysicsUtils.setScaledCollider(beef, 0.6f, 0.3f);
         beef.getComponent(ColliderComponent.class).setDensity(1.5f);
@@ -60,7 +62,8 @@ public class ItemFactory {
      */
     public static Entity createBanana(String chopLevel) {
         Entity banana = createTemplateItem()
-                .addComponent(new IngredientComponent("Banana", ItemType.BANANA, 1, 3, chopLevel))
+                .addComponent(new IngredientComponent("Banana", ItemType.BANANA, 1, 3,
+                        10, chopLevel))
                 .addComponent(new TextureRenderComponent(String.format("images/%s_banana.png", chopLevel)));
         PhysicsUtils.setScaledCollider(banana, 0.6f, 0.3f);
         banana.getComponent(ColliderComponent.class).setDensity(1.5f);
@@ -75,7 +78,8 @@ public class ItemFactory {
      */
     public static Entity createCucumber(String chopLevel) {
         Entity cucumber = createTemplateItem()
-                .addComponent(new IngredientComponent("Cucumber", ItemType.CUCUMBER, 1, 5, chopLevel))
+                .addComponent(new IngredientComponent("Cucumber", ItemType.CUCUMBER, 1, 5,
+                        10, chopLevel))
                 .addComponent(new TextureRenderComponent(String.format("images/%s_cucumber.png", chopLevel)));
         PhysicsUtils.setScaledCollider(cucumber, 0.6f, 0.3f);
         cucumber.getComponent(ColliderComponent.class).setDensity(1.5f);
@@ -90,7 +94,8 @@ public class ItemFactory {
      */
     public static Entity createTomato(String chopLevel) {
         Entity tomato = createTemplateItem()
-                .addComponent(new IngredientComponent("Tomato", ItemType.TOMATO, 1, 6, chopLevel))
+                .addComponent(new IngredientComponent("Tomato", ItemType.TOMATO, 1, 6,
+                        10, chopLevel))
                 .addComponent(new TextureRenderComponent(String.format("images/%s_tomato.png", chopLevel)));
         PhysicsUtils.setScaledCollider(tomato, 0.6f, 0.3f);
         tomato.getComponent(ColliderComponent.class).setDensity(1.5f);
@@ -105,7 +110,8 @@ public class ItemFactory {
      */
     public static Entity createStrawberry(String chopLevel) {
         Entity strawberry = createTemplateItem()
-                .addComponent(new IngredientComponent("Strawberry", ItemType.STRAWBERRY, 1, 3, chopLevel))
+                .addComponent(new IngredientComponent("Strawberry", ItemType.STRAWBERRY, 1, 3,
+                        10, chopLevel))
                 .addComponent(new TextureRenderComponent(String.format("images/%s_tomato.png", chopLevel)));
         PhysicsUtils.setScaledCollider(strawberry, 0.6f, 0.3f);
         strawberry.getComponent(ColliderComponent.class).setDensity(1.5f);
@@ -120,7 +126,7 @@ public class ItemFactory {
      */
     public static Entity createLettuce(String chopLevel) {
         Entity lettuce = createTemplateItem()
-                .addComponent(new IngredientComponent("Lettuce", ItemType.LETTUCE, 1, 10, chopLevel))
+                .addComponent(new IngredientComponent("Lettuce", ItemType.LETTUCE, 1, 10, 0, chopLevel))
                 .addComponent(new TextureRenderComponent(String.format("images/%s_tomato.png", chopLevel)));
         PhysicsUtils.setScaledCollider(lettuce, 0.6f, 0.3f);
         lettuce.getComponent(ColliderComponent.class).setDensity(1.5f);
@@ -133,8 +139,8 @@ public class ItemFactory {
      * @return A fruitSalad entity.
      */
     public static Entity createFruitSalad() {
-        List<IngredientComponent> ingredients = Arrays.asList(new IngredientComponent("Banana", ItemType.BANANA, 1, 3, "chopped"),
-                                                            new IngredientComponent("Strawberry", ItemType.STRAWBERRY, 1, 3, "chopped"));
+        List<IngredientComponent> ingredients = Arrays.asList(new IngredientComponent("Banana", ItemType.BANANA, 1, 3, 10,"chopped"),
+                                                            new IngredientComponent("Strawberry", ItemType.STRAWBERRY, 1, 3, 10, "chopped"));
 
         Entity fruitSalad = createTemplateItem()
                 .addComponent(new TextureRenderComponent("images/fruit_salad.png"))
