2d9adb2432226aac400c1e189cf085b649f52611
willembor <w.bor@uqconnect.edu.au>
2024-08-28 09:33:00 +1000

    Added tests for CookingComponent

--- source/core/src/test/com/csse3200/game/components/station/CookingComponentTest.java
+++ source/core/src/test/com/csse3200/game/components/station/CookingComponentTest.java
@@ -1,50 +1,80 @@
 package com.csse3200.game.components.station;
 
-import com.csse3200.game.components.player.InventoryComponent;
-import com.csse3200.game.entities.Entity;
 import com.csse3200.game.events.EventHandler;
 import com.csse3200.game.extensions.GameExtension;
 import com.csse3200.game.services.GameTime;
+import com.csse3200.game.entities.Entity;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
+import org.mockito.MockitoAnnotations;
 
-import java.util.*;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(GameExtension.class)
 public class CookingComponentTest {
-/**
+
+    private CookingComponent cookingComponent;
+    private StationInventoryComponent mockInventory;
+    private StationItemHandlerComponent itemHandle;
+    private GameTime mockTime;
+    private Entity mockEntity;
+    private EventHandler mockEvents;
+
     @BeforeEach
     void init() {
-        cookingComponent = new CookingComponent("OVEN");
+        MockitoAnnotations.openMocks(this);
+        ArrayList<String> acceptableItems = new ArrayList<>();
+        acceptableItems.add("acai");
+        acceptableItems.add("banana");
+        itemHandle = new StationItemHandlerComponent("OVEN", acceptableItems);
+        mockInventory = mock(StationInventoryComponent.class);
+
+        mockEntity = mock(Entity.class);
+        mockEvents = mock(EventHandler.class);
+
+        when(mockEntity.getComponent(StationInventoryComponent.class)).thenReturn(mockInventory);
+        when(mockEntity.getEvents()).thenReturn(mockEvents);
+
+        itemHandle.setEntity(mockEntity);
+        itemHandle.create();
+
+        cookingComponent = new CookingComponent();
+        cookingComponent.setEntity(mockEntity);
         cookingComponent.create();
     }
 
     @Test
-    void testAddItem() {
-        // Testing to be implemented
-    }
-
-    @Test
     void testRemoveItem() {
-        // Testing to be implemented
+        cookingComponent.addItem();
+        assertTrue(cookingComponent.isCooking());
+        cookingComponent.removeItem();
+        assertFalse(cookingComponent.isCooking());
     }
 
     @Test
     void testGetStationType() {
-        // Testing to be implemented
+        assertEquals("OVEN", itemHandle.getType());
     }
 
+
     @Test
-    void testIsCooking() {
-        // Testing to be implemented
+    void testUpdateCooking() {
+        cookingComponent.addItem();
+        when(mockTime.getDeltaTime()).thenReturn((float) 500L);
+        cookingComponent.update();
+        assertEquals(9500L, cookingComponent.getCookingTime());
+
+        cookingComponent.update();
+        assertEquals(9000, cookingComponent.getCookingTime());
     }
 
     @Test
     void testGetCookingTime() {
-        // Testing to be implemented
+        cookingComponent.addItem();
+        assertEquals(10000, cookingComponent.getCookingTime());
     }
-    **/
 }
