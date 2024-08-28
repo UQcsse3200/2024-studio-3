e233629b41d3be894fb66ababff83e80dd502ec4
Jordan Russo <jordan.russo@student.uq.edu.au>
2024-08-28 13:41:48 +1000

    Added ItemFactoryTest testing class

---
+++ source/core/src/test/com/csse3200/game/components/items/ItemFactoryTest.java
@@ -0,0 +1,12 @@
+package com.csse3200.game.components.items;
+
+import com.csse3200.game.components.items.*;
+import com.csse3200.game.entities.factories.ItemFactory;
+import com.csse3200.game.extensions.GameExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ItemFactoryTest {
+}
