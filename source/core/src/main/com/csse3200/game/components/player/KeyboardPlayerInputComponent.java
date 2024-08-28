54c202a152b11ae99532729c4f73efda29cfd8cf
bhavya malik <s4748840@student.uq.edu.au>
2024-08-29 09:23:44 +1000

    Fixed movement bug when interacting with a station.

--- source/core/src/main/com/csse3200/game/components/player/KeyboardPlayerInputComponent.java
+++ source/core/src/main/com/csse3200/game/components/player/KeyboardPlayerInputComponent.java
@@ -3,6 +3,7 @@ package com.csse3200.game.components.player;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.math.Vector2;
+import com.csse3200.game.components.SensorComponent;
 import com.csse3200.game.input.InputComponent;
 import com.csse3200.game.utils.math.Vector2Utils;
 
@@ -12,6 +13,7 @@ import com.csse3200.game.utils.math.Vector2Utils;
  */
 public class KeyboardPlayerInputComponent extends InputComponent {
   private final Vector2 walkDirection = Vector2.Zero.cpy();
+  private boolean isInteracting = false;
 
   public KeyboardPlayerInputComponent() {
     super(5);
@@ -25,33 +27,41 @@ public class KeyboardPlayerInputComponent extends InputComponent {
    */
   @Override
   public boolean keyDown(int keycode) {
-    switch (keycode) {
-      case Keys.W:
-        walkDirection.add(Vector2Utils.UP);
-        triggerWalkEvent();
-        return true;
-      case Keys.A:
-        walkDirection.add(Vector2Utils.LEFT);
-        triggerWalkEvent();
-        return true;
-      case Keys.S:
-        walkDirection.add(Vector2Utils.DOWN);
-        triggerWalkEvent();
-        return true;
-      case Keys.D:
-        walkDirection.add(Vector2Utils.RIGHT);
-        triggerWalkEvent();
-        return true;
-      case Keys.SPACE:
-        entity.getEvents().trigger("attack");
-        return true;
-      case Keys.E:
-        entity.getEvents().trigger("interact");
-        return true;
-      default:
-        return false;
+    if (keycode == Keys.E) {
+      // Trigger an interaction attempt
+      entity.getEvents().trigger("interact");
+      return true;
     }
+
+    if (!isInteracting) {
+      switch (keycode) {
+        case Keys.W:
+          walkDirection.add(Vector2Utils.UP);
+          triggerWalkEvent();
+          return true;
+        case Keys.A:
+          walkDirection.add(Vector2Utils.LEFT);
+          triggerWalkEvent();
+          return true;
+        case Keys.S:
+          walkDirection.add(Vector2Utils.DOWN);
+          triggerWalkEvent();
+          return true;
+        case Keys.D:
+          walkDirection.add(Vector2Utils.RIGHT);
+          triggerWalkEvent();
+          return true;
+      }
+    }
+
+    if (keycode == Keys.SPACE) {
+      entity.getEvents().trigger("attack");
+      return true;
+    }
+
+    return false;
   }
+
 
   /**
    * Triggers player events on specific keycodes.
@@ -61,26 +71,27 @@ public class KeyboardPlayerInputComponent extends InputComponent {
    */
   @Override
   public boolean keyUp(int keycode) {
-    switch (keycode) {
-      case Keys.W:
-        walkDirection.sub(Vector2Utils.UP);
-        triggerWalkEvent();
-        return true;
-      case Keys.A:
-        walkDirection.sub(Vector2Utils.LEFT);
-        triggerWalkEvent();
-        return true;
-      case Keys.S:
-        walkDirection.sub(Vector2Utils.DOWN);
-        triggerWalkEvent();
-        return true;
-      case Keys.D:
-        walkDirection.sub(Vector2Utils.RIGHT);
-        triggerWalkEvent();
-        return true;
-      default:
-        return false;
+    if (!isInteracting) {
+      switch (keycode) {
+        case Keys.W:
+          walkDirection.sub(Vector2Utils.UP);
+          triggerWalkEvent();
+          return true;
+        case Keys.A:
+          walkDirection.sub(Vector2Utils.LEFT);
+          triggerWalkEvent();
+          return true;
+        case Keys.S:
+          walkDirection.sub(Vector2Utils.DOWN);
+          triggerWalkEvent();
+          return true;
+        case Keys.D:
+          walkDirection.sub(Vector2Utils.RIGHT);
+          triggerWalkEvent();
+          return true;
+      }
     }
+    return false;
   }
 
   private void triggerWalkEvent() {
@@ -90,4 +101,29 @@ public class KeyboardPlayerInputComponent extends InputComponent {
       entity.getEvents().trigger("walk", walkDirection);
     }
   }
+
+  @Override
+  public void create() {
+    super.create();
+    entity.getEvents().addListener("interactionEnd", this::whenInteractionEnds);
+    entity.getEvents().addListener("startInteraction", this::startInteraction);
+  }
+
+  /**
+   * Called when the player is attempting to interact and there is also a station that can be interacted with
+   */
+  private void startInteraction() {
+    isInteracting = !isInteracting;
+  
+    if (isInteracting) {
+      walkDirection.set(Vector2.Zero); 
+      triggerWalkEvent();             
+    }
+  }
+
+  private void whenInteractionEnds() {
+    isInteracting = false;
+  }
+
+
 }
