1e7c7fcd75ba656bc7a50aa99ee92bea0f0587a4
Madeleine Coombs <madeleinecoombs@outlook.com>
2024-08-28 16:10:27 +1000

    Reworks direction of MealComponent and adds deleteIngredient(). Related to #47

--- source/core/src/main/com/csse3200/game/components/items/MealComponent.java
+++ source/core/src/main/com/csse3200/game/components/items/MealComponent.java
@@ -6,15 +6,13 @@ public class MealComponent extends ItemComponent {
     private List<IngredientComponent> ingredients;
     private int quality;
     private int price;
-    private String mealType;
 
-    public MealComponent(String itemName, ItemType itemType, int weight, List<IngredientComponent> ingredients,
+    public MealComponent(String mealName, ItemType mealType, int weight, List<IngredientComponent> ingredients,
                          int price) {
-        super(itemName, itemType, weight);
+        super(mealName, mealType, weight);
         this.ingredients = ingredients;
         this.quality = calculateQuality();
         this.price = price;
-        this.mealType = determineMealType();
 
     }
 
@@ -24,12 +22,14 @@ public class MealComponent extends ItemComponent {
 
     public void setIngredients(List<IngredientComponent> ingredients) {
         this.ingredients = ingredients;
-        this.mealType = determineMealType();
     }
 
     public void addIngredient(IngredientComponent ingredient) {
         this.ingredients.add(ingredient);
-        this.mealType = determineMealType();
+    }
+
+    public void deleteIngredient(IngredientComponent ingredient) {
+        this.ingredients.remove(ingredient);
     }
 
     private int calculateQuality() {
@@ -48,14 +48,6 @@ public class MealComponent extends ItemComponent {
         penalty = (penaltyCount / ingredients.size()) * 100;
 
         return qualityScore - penalty;
-    }
-
-    private String determineMealType() {
-        return "SomeMealType";
-    }
-
-    public String getMealType() {
-        return mealType;
     }
 
     public int getQuality() {
