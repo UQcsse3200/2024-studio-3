60086faeee1a60fafdf28a947910f1f0e1880dfe
ImLeiSu <suleisui1768@gmail.com>
2024-08-29 11:41:29 +1000

    Update EndDayDisplay.java UI

--- source/core/src/main/com/csse3200/game/components/maingame/EndDayDisplay.java
+++ source/core/src/main/com/csse3200/game/components/maingame/EndDayDisplay.java
@@ -1,23 +1,34 @@
 package com.csse3200.game.components.maingame;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.List;
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.csse3200.game.GdxGame;
 import com.csse3200.game.screens.MainGameScreen;
+import com.csse3200.game.services.ServiceLocator;
 import com.csse3200.game.ui.UIComponent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
 
 public class EndDayDisplay extends UIComponent {
     private Table layout; // Layout manager
     private boolean isVisible;
     private final MainGameScreen game;
+    private Image dayEndImage;
 
     public EndDayDisplay(MainGameScreen game) {
         super();
@@ -32,6 +43,22 @@ public class EndDayDisplay extends UIComponent {
         layout.setVisible(isVisible);
         stage.addActor(layout);
 
+        // Create a white background
+        Pixmap pixmap = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
+        pixmap.setColor(Color.WHITE);
+        pixmap.fill();
+        Texture pixmapTex = new Texture(pixmap);
+        pixmap.dispose();
+        Drawable whiteBackground = new TextureRegionDrawable(new TextureRegion(pixmapTex));
+        layout.setBackground(whiteBackground);
+
+        // Load the image
+        Texture imgTexture = ServiceLocator.getResourceService()
+                .getAsset("images/bird.png", Texture.class);
+        Drawable imgDrawable = new TextureRegionDrawable(imgTexture);
+        dayEndImage = new Image(imgDrawable);
+        layout.add(dayEndImage).center().padTop(10).row();
+
         initializeUI();
         setupInputListener();
     }
@@ -39,6 +66,24 @@ public class EndDayDisplay extends UIComponent {
     private void initializeUI() {
         Label titleLabel = new Label("Day End Summary", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
         layout.add(titleLabel).pad(10).row();
+
+        // Example event labels
+        Label eventLabel = new Label("Major Events in the Kitchen Game Today", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        layout.add(eventLabel).pad(10).row();
+
+        // Customer lists
+        List<String> passedCustomers = new List<>(skin);
+        passedCustomers.setItems("Customer A", "Customer B", "Customer C");
+        List<String> failedCustomers = new List<>(skin);
+        failedCustomers.setItems("Customer X", "Customer Y");
+
+        Table listTable = new Table();
+        listTable.add(new Label("Passed Customers", skin)).pad(10);
+        listTable.add(new Label("Failed Customers", skin)).pad(10).row();
+        listTable.add(new ScrollPane(passedCustomers, skin)).pad(10);
+        listTable.add(new ScrollPane(failedCustomers, skin)).pad(10);
+
+        layout.add(listTable).expand().fill().row();
 
         TextButton closeBtn = new TextButton("Close", skin);
         closeBtn.addListener(new ClickListener() {
@@ -84,8 +129,9 @@ public class EndDayDisplay extends UIComponent {
     }
 
     @Override
-    public void draw(SpriteBatch spriteBatch) {
-        // Drawing logic if needed
+    public void draw(SpriteBatch batch) {
+        // draw is handled by the stage
+
     }
 
     @Override
