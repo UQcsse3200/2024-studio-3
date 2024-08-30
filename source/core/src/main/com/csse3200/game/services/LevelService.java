f623e00f9ef09f7d7f2456b715a7839bc20ff3d5
Michael Kidd <4michaelkidd@gmail.com>
2024-08-30 20:54:02 +1000

    Added new private variable currLevel for tracking the current level of the game, as well as new methods for retrieving and setting currLevel

--- source/core/src/main/com/csse3200/game/services/LevelService.java
+++ source/core/src/main/com/csse3200/game/services/LevelService.java
@@ -6,12 +6,22 @@ import java.util.logging.Level;
 
 public class LevelService {
     private final EventHandler levelEventHandler;
+    private int currLevel;
 
     public LevelService() {
         levelEventHandler = new EventHandler();
+        currLevel = 1;
     }
 
     public EventHandler getEvents() {
         return levelEventHandler;
+    }
+
+    public int getCurrLevel() {
+        return currLevel;
+    }
+
+    public void incrementLevel() {
+        currLevel++;
     }
 }
