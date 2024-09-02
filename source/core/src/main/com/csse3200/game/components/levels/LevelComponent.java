c1734001c6ed5cd772c4292e33e29fc11e86582f
Michael Kidd <4michaelkidd@gmail.com>
2024-09-02 18:20:57 +1000

    Created new methods in LevelComponent class, which handle the private variables used to track the spawning stats of the current level

--- source/core/src/main/com/csse3200/game/components/levels/LevelComponent.java
+++ source/core/src/main/com/csse3200/game/components/levels/LevelComponent.java
@@ -1,4 +1,43 @@
 package com.csse3200.game.components.levels;
 
-public class LevelComponent {
+import com.badlogic.gdx.utils.TimeUtils;
+import com.csse3200.game.components.Component;
+
+public class LevelComponent extends Component {
+    private long spawnStartTime = TimeUtils.millis();
+    private boolean nowSpawning = false;
+    private int levelSpawnCap = 0;
+    private int currentCustomersLinedUp = 0;
+
+    @Override
+    public void update() {
+        if (nowSpawning) {
+            ;
+        }
+    }
+
+    public void initSpawning(int spawnCap) {
+        setLevelSpawnCap(spawnCap);
+        toggleNowSpawning();
+    }
+
+    public void toggleNowSpawning() {
+        nowSpawning = !nowSpawning;
+    }
+
+    public void setLevelSpawnCap(int cap) {
+        levelSpawnCap = cap;
+    }
+
+    public int getCurrentCustomersLinedUp() {
+        return currentCustomersLinedUp;
+    }
+
+    public void customerJoinedLineUp() {
+        currentCustomersLinedUp++;
+    }
+
+    public void customerLeftLineUp() {
+        currentCustomersLinedUp--;
+    }
 }
