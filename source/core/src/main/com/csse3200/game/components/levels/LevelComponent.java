e52d412bbeab783cbea5865a41d6edef6339c5e1
Michael Kidd <4michaelkidd@gmail.com>
2024-09-02 18:32:49 +1000

    Added more public methods for controlling the private variables in the LevelComponent class. Began properly implementing the update() method with al l the required checks included and some of the method calls required for appropriate tracking

--- source/core/src/main/com/csse3200/game/components/levels/LevelComponent.java
+++ source/core/src/main/com/csse3200/game/components/levels/LevelComponent.java
@@ -4,21 +4,39 @@ import com.badlogic.gdx.utils.TimeUtils;
 import com.csse3200.game.components.Component;
 
 public class LevelComponent extends Component {
-    private long spawnStartTime = TimeUtils.millis();
+    private long spawnStartTime = 0;
     private boolean nowSpawning = false;
     private int levelSpawnCap = 0;
+    private int numbCustomersSpawned = 0;
     private int currentCustomersLinedUp = 0;
 
     @Override
     public void update() {
         if (nowSpawning) {
-            ;
+            long elapsedTime = TimeUtils.timeSinceMillis(spawnStartTime);
+            long elapsedTimeSecs = elapsedTime / 1000;
+            //If more than five seconds have passed, there are more customers to spawn
+            //AND if another customer can join the line
+            if (elapsedTimeSecs >= 5 && numbCustomersSpawned < levelSpawnCap && currentCustomersLinedUp < 5) {
+                setSpawnStartTime();
+                customerSpawned();
+                //spawn customer lol
+                if (numbCustomersSpawned == levelSpawnCap) {
+                    toggleNowSpawning();
+                }
+            }
         }
     }
 
     public void initSpawning(int spawnCap) {
+        resetCustomerSpawn();
         setLevelSpawnCap(spawnCap);
+        setSpawnStartTime();
         toggleNowSpawning();
+    }
+
+    public void setSpawnStartTime() {
+        spawnStartTime = TimeUtils.millis();
     }
 
     public void toggleNowSpawning() {
@@ -39,5 +57,13 @@ public class LevelComponent extends Component {
 
     public void customerLeftLineUp() {
         currentCustomersLinedUp--;
+    }
+
+    public void customerSpawned() {
+        numbCustomersSpawned++;
+    }
+
+    public void resetCustomerSpawn() {
+        numbCustomersSpawned = 0;
     }
 }
