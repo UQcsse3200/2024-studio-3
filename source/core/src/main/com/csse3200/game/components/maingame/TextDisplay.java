1f3e5e1e7f8c86eecdbbeab9adebc4ef89dc4311
jackson van duikeren <jackovand27@icloud.com>
2024-09-04 10:34:07 +1000

    Fixed imports and added required base methods to TextDisplay

--- source/core/src/main/com/csse3200/game/components/maingame/TextDisplay.java
+++ source/core/src/main/com/csse3200/game/components/maingame/TextDisplay.java
@@ -1,5 +1,28 @@
 package com.csse3200.game.components.maingame;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.List;
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.csse3200.game.GdxGame;
+import com.csse3200.game.screens.MainGameScreen;
+import com.csse3200.game.services.ServiceLocator;
+import com.csse3200.game.ui.UIComponent;
 public class TextDisplay extends UIComponent {
     private boolean isVisible;
     private final MainGameScreen game;
@@ -16,4 +39,9 @@ public class TextDisplay extends UIComponent {
     public String getText() {
         return text;
     }
+
+    @Override
+    public void draw(SpriteBatch batch) {}
+    @Override
+    public void setStage(Stage mock) {}
 }
