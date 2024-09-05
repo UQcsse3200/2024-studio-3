eb1f5eacdeb7fd1ec926b4a094dbd0d461e3b54b
Michael Kidd <4michaelkidd@gmail.com>
2024-09-06 00:08:54 +1000

    Created new Test file named LevelServiceTest in

---
+++ source/core/src/test/com/csse3200/game/services/LevelServiceTest.java
@@ -0,0 +1,11 @@
+package com.csse3200.game.services;
+
+import com.csse3200.game.extensions.GameExtension;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(GameExtension.class)
+@ExtendWith(MockitoExtension.class)
+public class LevelServiceTest {
+}
