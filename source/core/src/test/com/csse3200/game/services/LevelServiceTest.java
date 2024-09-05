8d5e99c8290b53f57f2b0512641b0aab0a0663a3
Michael Kidd <4michaelkidd@gmail.com>
2024-09-06 00:40:42 +1000

    Created first test in LevelServiceTest, testing if the service is created properly. Need to figure out how to add tests to gradle...

--- source/core/src/test/com/csse3200/game/services/LevelServiceTest.java
+++ source/core/src/test/com/csse3200/game/services/LevelServiceTest.java
@@ -1,11 +1,33 @@
 package com.csse3200.game.services;
 
 import com.csse3200.game.extensions.GameExtension;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(GameExtension.class)
 @ExtendWith(MockitoExtension.class)
-public class LevelServiceTest {
+class LevelServiceTest {
+    @Spy LevelService levelServiceSpy;
+
+    /*@BeforeAll
+    void initSetUp() {
+        ServiceLocator.registerLevelService(levelServiceSpy);
+    }*/
+
+    @Test
+    void shouldInitialiseProperlyWhenCreated() {
+        LevelService levelService = new LevelService();
+        assertNotNull(levelService.getCurrLevel());
+        assertNotNull(levelService.getEvents());
+        levelServiceSpy.getEvents().trigger("startLevel");
+        levelServiceSpy.getEvents().trigger("createCustomer");
+        verify(levelServiceSpy).getEvents().trigger("startLevel");
+        verify(levelServiceSpy).getEvents().trigger("createCustomer");
+    }
 }
