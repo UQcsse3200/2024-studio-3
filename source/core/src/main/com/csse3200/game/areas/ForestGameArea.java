85be53a000d572f782fa863ed05628a55a81619f
Marcus Baulch <m.baulch@uq.net.au>
2024-09-05 16:55:35 +1000

    Gorilla now works in-game and implemented its idle animation while it's walking, replaced the other animal images with their sprite sheets

--- source/core/src/main/com/csse3200/game/areas/ForestGameArea.java
+++ source/core/src/main/com/csse3200/game/areas/ForestGameArea.java
@@ -88,7 +88,9 @@ public class ForestGameArea extends GameArea {
     "images/stations/benches/bench6-top.png"
   };
   private static final String[] forestTextureAtlases = {
-    "images/terrain_iso_grass.atlas", "images/ghost.atlas", "images/ghostKing.atlas", "images/animal_images/gorilla.atlas"
+    "images/terrain_iso_grass.atlas", "images/ghost.atlas", "images/ghostKing.atlas", "images/animal_images/gorilla.atlas",
+          "images/animal_images/goose.atlas", "images/animal_images/goat.atlas", "images/animal_images/monkey.atlas",
+          "images/animal_images/snow_wolf.atlas"
   };
   private static final String[] forestSounds = {"sounds/Impact4.ogg"};
   private static final String backgroundMusic = "sounds/BGM_03_mp3.mp3";
