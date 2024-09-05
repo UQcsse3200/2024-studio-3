f97ca68529fc2d3f7e39ac24f25f2a49eb3f7392
Caleb Murphy <calebmurphy2003@gmail.com>
2024-09-05 10:50:50 +1000

    #112 Flame rendered to screen with animation

--- source/core/src/main/com/csse3200/game/areas/ForestGameArea.java
+++ source/core/src/main/com/csse3200/game/areas/ForestGameArea.java
@@ -92,7 +92,7 @@ public class ForestGameArea extends GameArea {
     "images/terrain_iso_grass.atlas",
     "images/ghost.atlas",
     "images/ghostKing.atlas",
-    "images/fireExtinguisher/atlas/flame.atlasd",
+    "images/fireExtinguisher/atlas/flame.atlas",
   };
   private static final String[] forestSounds = {"sounds/Impact4.ogg"};
   private static final String backgroundMusic = "sounds/BGM_03_mp3.mp3";
