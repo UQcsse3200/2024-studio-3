5d1f128ec659287b50395a437cea70d6feb56b88
Michael Kidd <4michaelkidd@gmail.com>
2024-09-07 00:35:18 +1000

    Fixed issue where for some god forsaken reason gradle was not running LevelServiceTest. Also fixed up some issues with the test itself

--- source/core/src/test/com/csse3200/game/services/LevelServiceTest.java
+++ source/core/src/test/com/csse3200/game/services/LevelServiceTest.java
@@ -1,19 +1,21 @@
 package com.csse3200.game.services;
 
+import com.csse3200.game.events.listeners.EventListener0;
 import com.csse3200.game.extensions.GameExtension;
-import org.junit.Test;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(GameExtension.class)
 @ExtendWith(MockitoExtension.class)
 class LevelServiceTest {
-    @Spy LevelService levelServiceSpy;
+    @Spy
+    LevelService levelServiceSpy;
 
     /*@BeforeAll
     void initSetUp() {
@@ -25,10 +27,14 @@ class LevelServiceTest {
         LevelService levelService = new LevelService();
         assertNotNull(levelService.getCurrLevel());
         assertNotNull(levelService.getEvents());
-        levelServiceSpy.getEvents().trigger("startLevel");
+        EventListener0 mockListener = mock(EventListener0.class);
+        /*levelServiceSpy.getEvents().trigger("startLevel", 1);
         levelServiceSpy.getEvents().trigger("createCustomer");
-        verify(levelServiceSpy).getEvents().trigger("startLevel");
-        verify(levelServiceSpy).getEvents().trigger("createCustomer");
+        verify(levelServiceSpy).getEvents().trigger("startLevel", 1);
+        verify(levelServiceSpy).getEvents().trigger("createCustomer");*/
+        levelServiceSpy.getEvents().addListener("mockEvent",mockListener);
+        levelServiceSpy.getEvents().trigger("mockEvent");
+        verify(mockListener).handle();
     }
 
     @Test
@@ -36,4 +42,5 @@ class LevelServiceTest {
         levelServiceSpy.getEvents();
         verify(levelServiceSpy).getEvents();
     }
+
 }
