436fb05210292a96d22685646460df4b963f1b33
Michael Kidd <4michaelkidd@gmail.com>
2024-09-07 16:09:20 +1000

    Added missing ExtendWith annotations and added one test

--- source/core/src/test/com/csse3200/game/components/levels/LevelComponentTest.java
+++ source/core/src/test/com/csse3200/game/components/levels/LevelComponentTest.java
@@ -1,18 +1,33 @@
 package com.csse3200.game.components.levels;
 
 import com.csse3200.game.entities.Entity;
+import com.csse3200.game.extensions.GameExtension;
 import com.csse3200.game.services.LevelService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
 
+@ExtendWith(GameExtension.class)
+@ExtendWith(MockitoExtension.class)
 class LevelComponentTest {
-    @Spy LevelComponent levelComponentSpy;
-    @Spy Entity entitySpy;
+    @Spy
+    LevelComponent levelComponentSpy;
+    @Spy
+    Entity entitySpy;
 
-    @BeforeEach
+    /*@BeforeEach
     void setUp() {
         entitySpy.addComponent(levelComponentSpy);
+    }*/
+
+    @Test
+    void shouldInitSpawningProperly() {
+        levelComponentSpy.initSpawning(1);
+        verify(levelComponentSpy).initSpawning(1);
     }
 }
