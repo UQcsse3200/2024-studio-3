95b50e3044083988f41298b22c47776ebd3dd691
Michael Kidd <4michaelkidd@gmail.com>
2024-09-07 15:59:44 +1000

    Created new Spy objects, and a BeforeEach set up for LevelComponentTest

--- source/core/src/test/com/csse3200/game/components/levels/LevelComponentTest.java
+++ source/core/src/test/com/csse3200/game/components/levels/LevelComponentTest.java
@@ -1,7 +1,18 @@
 package com.csse3200.game.components.levels;
 
+import com.csse3200.game.entities.Entity;
+import com.csse3200.game.services.LevelService;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Spy;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LevelComponentTest {
+    @Spy LevelComponent levelComponentSpy;
+    @Spy Entity entitySpy;
 
+    @BeforeEach
+    void setUp() {
+        entitySpy.addComponent(levelComponentSpy);
+    }
 }
