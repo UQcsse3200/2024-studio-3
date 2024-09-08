9422f71390dacf30c62f2d0886ce19efa461f798
V4N5HM <s4820705@student.uq.edu.au>
2024-09-08 18:28:47 +1000

    Fixing unit test

--- source/core/src/test/com/csse3200/game/components/tasks/PathFollowTaskTest.java
+++ source/core/src/test/com/csse3200/game/components/tasks/PathFollowTaskTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.verify;
 import com.csse3200.game.ai.tasks.AITaskComponent;
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.events.listeners.EventListener0;
+import com.csse3200.game.events.listeners.EventListener1;
 import com.csse3200.game.extensions.GameExtension;
 import com.csse3200.game.utils.math.Vector2Utils;
 import com.csse3200.game.physics.components.PhysicsMovementComponent;
@@ -30,20 +31,47 @@ class PathFollowTaskTest {
     }
 
     @Test
-    void shouldTriggerEvent() {
+    void shouldTriggerWanderStartEvent() {
         Vector2 targetPosition = new Vector2(5f, 5f);
-        PathFollowTask pathFollowTask = new PathFollowTask(targetPosition);
+        String customerId = "testCustomerId"; // Use a sample customer ID
+        PathFollowTask pathFollowTask = new PathFollowTask(targetPosition, customerId);
 
         AITaskComponent aiTaskComponent = new AITaskComponent().addTask(pathFollowTask);
         Entity entity = new Entity().addComponent(aiTaskComponent).addComponent(new PhysicsMovementComponent());
         entity.create();
 
         // Register callbacks
-        EventListener0 callback = mock(EventListener0.class);
-        entity.getEvents().addListener("wanderStart", callback);
+        EventListener0 wanderStartCallback = mock(EventListener0.class);
+        entity.getEvents().addListener("wanderStart", wanderStartCallback);
 
         pathFollowTask.start();
 
-        verify(callback).handle();
+        // Verify that wanderStart event was triggered
+        verify(wanderStartCallback).handle();
+    }
+
+    @Test
+    void shouldTriggerLeaveEarlyEvent() {
+        Vector2 targetPosition = new Vector2(5f, 5f);
+        String customerId = "testCustomerId";
+        PathFollowTask pathFollowTask = new PathFollowTask(targetPosition, customerId);
+
+        AITaskComponent aiTaskComponent = new AITaskComponent().addTask(pathFollowTask);
+        Entity entity = new Entity().addComponent(aiTaskComponent).addComponent(new PhysicsMovementComponent());
+        entity.create();
+
+        // Register callbacks
+        EventListener1 leaveEarlyCallback = mock(EventListener1.class);
+        entity.getEvents().addListener("leaveEarly", leaveEarlyCallback);
+
+        // Simulate an early leave event
+        entity.getEvents().trigger("leaveEarly", customerId);
+
+        // Update task to ensure it's executed
+        pathFollowTask.start();
+        pathFollowTask.update();
+
+        // Verify that leaveEarly event was triggered
+        verify(leaveEarlyCallback).handle(customerId);
     }
 }
