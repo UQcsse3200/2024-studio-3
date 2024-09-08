05d4e13e1ca41a319e5a695a9e48783d0f45ce01
ashom2 <alekid804@gmail.com>
2024-09-08 13:20:06 +1000

    Added methods from CookingComponent to merge

--- source/core/src/main/com/csse3200/game/components/station/StationInventoryComponent.java
+++ source/core/src/main/com/csse3200/game/components/station/StationInventoryComponent.java
@@ -1,9 +1,13 @@
 package com.csse3200.game.components.station;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.ArrayList;
 
 import com.csse3200.game.components.Component;
+import com.csse3200.game.entities.factories.DishFactory;
+import com.csse3200.game.services.GameTime;
+import com.csse3200.game.services.ServiceLocator;
 
 
 /**
@@ -18,6 +22,10 @@ public class StationInventoryComponent extends Component {
      */
     private static final int SIZE = 2;
     private ArrayList<Optional<String>> item;
+    private GameTime timeSource;
+    private long cookingTime;
+    private boolean isCooking;
+    private String targetRecipe;
 
 
     /**
@@ -33,6 +41,35 @@ public class StationInventoryComponent extends Component {
         this.item = new ArrayList<>(SIZE);
         this.item.add(Optional.empty());
         this.item.add(Optional.empty());
+    }
+
+    /**
+     * Called on creation of the station and adds listeners.
+     */
+    @Override
+    public void create() {
+        timeSource = ServiceLocator.getTimeSource();
+        entity.getEvents().addListener("give station item", this::addItem);
+        entity.getEvents().addListener("take item", this::removeItem);
+    }
+
+    /**
+     * Called every frame to incrementally cook items, scaled by time scale.
+     */
+    @Override
+    public void update() {
+        // Add to cooking timer and cook item
+        if (isCooking) {
+            if (cookingTime < 0) { // Recipe is fully cooked
+                // remove current items
+                for (Optional<String> x : getItems()) {
+                    removeCurrentItem();
+                }
+                // replace with dish from recipe
+                setCurrentItem(targetRecipe);
+            }
+            cookingTime -= timeSource.getDeltaTime();
+        }
     }
 
     /**
@@ -67,7 +104,6 @@ public class StationInventoryComponent extends Component {
         // No item present, set item
         this.item.set(0, (Optional.of(newItem)));
         return true;
-
     }
 
     /**
@@ -97,10 +133,55 @@ public class StationInventoryComponent extends Component {
     }
 
     /**
+     * Method triggered when item added to station.
+     */
+    public void addItem(String item) {
+        // Converting List<Optional<String>> to List<String>
+        List<String> templist = new ArrayList<String>();
+        for (Optional<String> x : getItems()) {
+            if (x.isPresent()) templist.add(x.get());
+        }
+
+        List<String> possibleRecipes = DishFactory.getRecipe(templist);
+        if (possibleRecipes.size() == 1 && !isCooking) {
+            // TODO check that it completely matching the recipe
+            targetRecipe = possibleRecipes.get(0);
+            cookingTime = 10000; // TODO edit placeholder, get cooking time from recipes?
+            isCooking = true;
+        }
+        else
+        {
+            // TODO
+        }
+    }
+
+    /**
      * Gets the items contained in the inventory.
      * @return the list of items
      */
     public ArrayList<Optional<String>> getItems() {
         return item;
+    }
+
+    /**
+     * Method triggered when item removed from station.
+     */
+    public void removeItem() {
+        isCooking = false;
+    }
+
+    /** @return true if the item is being cooked, false otherwise. */
+    public boolean isCooking() {
+        return isCooking;
+    }
+
+    /** @return time remaining to make the recipe in seconds, scaled by time scale. */
+    public long getCookingTime() {
+        return cookingTime;
+    }
+
+    /** @return the name of the target recipe */
+    public String getTargetRecipe() {
+        return targetRecipe;
     }
 }
