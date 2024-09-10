aba7efe05b9a1ce7ef466e58dc443f3951de4eb7
amberchen <s4810145@student.uq.edu.au>
2024-09-11 08:57:56 +1000

    implemented timer (doesnt update screen per second) and create test file

--- source/core/src/main/com/csse3200/game/screens/MoralDecisionDisplay.java
+++ source/core/src/main/com/csse3200/game/screens/MoralDecisionDisplay.java
@@ -7,23 +7,29 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.TimeUtils;
+import com.csse3200.game.components.Component;
+import com.csse3200.game.entities.Entity;
 import com.csse3200.game.services.ServiceLocator;
 import com.csse3200.game.ui.UIComponent;
-
 
 public class MoralDecisionDisplay extends UIComponent {
     private Table layout; // Layout manager
     private boolean isVisible;
     private final MainGameScreen game;
+
     private Image characterImage;
+    private Label timerLabel; // Timer label
+    private long startTime; // Track the start time
+
+    private static final long DEFAULT_TIMER = 10000; // 10 seconds (in milliseconds)
+    private long remainingTime; // Remaining time in milliseconds
 
     public MoralDecisionDisplay(MainGameScreen game) {
         super();
@@ -41,13 +47,13 @@ public class MoralDecisionDisplay extends UIComponent {
     public void create() {
         super.create();
 
-        // create a table layout
+        // Create a table layout
         layout = new Table();
         layout.setFillParent(true);
         layout.setVisible(isVisible);
         stage.addActor(layout);
 
-        // create gray background
+        // Create gray background
         Pixmap pixmap = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
         pixmap.setColor(Color.GRAY);
         pixmap.fill();
@@ -56,49 +62,32 @@ public class MoralDecisionDisplay extends UIComponent {
         Drawable blackBackground = new TextureRegionDrawable(new TextureRegion(pixmapTex));
         layout.setBackground(blackBackground);
 
-        // set up the label using table layout
+        // Set up the label using table layout
         BitmapFont font = new BitmapFont();
         Label titleLabel = new Label("Moral Decision", new Label.LabelStyle(font, Color.WHITE));
         layout.add(titleLabel).pad(10).row();
 
-        // load and position the racoon image slightly to the left
+        // Load and position the raccoon image slightly to the left
         Texture imgTexture = new Texture(Gdx.files.internal("images/racoon.png"));
         Drawable imgDrawable = new TextureRegionDrawable(imgTexture);
         characterImage = new Image(imgDrawable);
 
-        // add racoon image to the table and shift it left by adjusting padding
+        // Add raccoon image to the table and shift it left by adjusting padding
         layout.add(characterImage).padRight(1000).center().row(); // Add padding to move left
 
-//        setupInputListener();
+        // Timer label setup - Initialize with default timer
+        timerLabel = new Label("Timer: " + (DEFAULT_TIMER / 1000) + "s", new Label.LabelStyle(font, Color.RED));
+        layout.add(timerLabel).padLeft(10f).row(); // Add timer label to the layout
+
         entity.getEvents().addListener("triggerMoralScreen", this::toggleVisibility);
     }
-
-
-//    private void setupInputListener() {
-//        stage.addListener(new InputListener() {
-//            @Override
-//            public boolean keyDown(InputEvent event, int keycode) {
-//                if (keycode == com.badlogic.gdx.Input.Keys.M) {
-//                    toggleVisibility();
-//                    return true;
-//                }
-//                return false;
-//            }
-//        });
-//    }
-
-
-    private void initialiseUI() {
-        Label titleLabel = new Label("moral deiciosn", new Label.LabelStyle(new BitmapFont(), Color.PINK));
-        layout.add(titleLabel).pad(10).row();
-
-    }
-
 
     public void show() {
         isVisible = true;
         layout.setVisible(isVisible);
         game.pause(); // Pause the game when the display is shown
+        startTime = TimeUtils.millis(); // Set the start time when shown
+        remainingTime = DEFAULT_TIMER; // Reset timer to the default value
     }
 
     public void hide() {
@@ -123,4 +112,28 @@ public class MoralDecisionDisplay extends UIComponent {
     @Override
     public void setStage(Stage stage) {
     }
+
+    @Override
+    public void update() {
+        // Only one timer and background in the moral decision, no need for arrays or loops
+        if (isVisible) {
+            // Calculate elapsed time since the moral decision screen was shown
+            long elapsedTime = TimeUtils.timeSinceMillis(startTime);
+
+            // Calculate remaining time by subtracting the elapsed time from the default timer
+            remainingTime = DEFAULT_TIMER - elapsedTime;
+
+            // Update the timer and background if there's still time left
+            if (remainingTime > 0) {
+                // Update the label with remaining time in seconds
+                timerLabel.setText("Timer: " + (remainingTime / 1000) + "s");
+
+                // Optionally, update the visual background here if needed
+            } else {
+                // If the time has expired, hide the moral decision screen
+                hide();
+            }
+        }
+    }
+
 }
---
+++ source/core/src/test/com/csse3200/game/screens/MoralDecisionDisplayTest.java
@@ -0,0 +1,12 @@
+package com.csse3200.game.screens;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class MoralDecisionDisplayTest {
+}
