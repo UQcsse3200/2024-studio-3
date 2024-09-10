75681a5a2ffbd8f4c6241f0709a7139d45431764
StrayIntellectual <andikajohn05@gmail.com>
2024-09-10 23:16:43 +1000

    Update ForestGameArea.java

--- source/core/src/main/com/csse3200/game/areas/ForestGameArea.java
+++ source/core/src/main/com/csse3200/game/areas/ForestGameArea.java
@@ -120,7 +120,7 @@ public class ForestGameArea extends GameArea {
   private static final String[] forestTextureAtlases = {
     "images/terrain_iso_grass.atlas", "images/ghost.atlas", "images/ghostKing.atlas", "images/animal_images/gorilla.atlas",
           "images/animal_images/goose.atlas", "images/animal_images/goat.atlas", "images/animal_images/monkey.atlas",
-          "images/animal_images/snow_wolf.atlas","images/player.atlas", "images/fireExtinguisher/atlas/flame.atlas"
+          "images/animal_images/snow_wolf.atlas","images/player.atlas", "images/fireExtinguisher/atlas/flame.atlas", "images/stations/oven/oven.atlas"
   };
   private static final String[] forestSounds = {"sounds/Impact4.ogg"};
   private static final String backgroundMusic = "sounds/BGM_03_mp3.mp3";
