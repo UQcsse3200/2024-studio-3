5813d03c2cf7a763c9758d78a045f631c26e7281
Joanna Wang <s4740105@student.uq.edu.au>
2024-09-12 13:38:15 +1000

    remove moral display

--- source/core/src/main/com/csse3200/game/screens/MainGameScreen.java
+++ source/core/src/main/com/csse3200/game/screens/MainGameScreen.java
@@ -207,8 +207,7 @@ public class MainGameScreen extends ScreenAdapter {
 			.addComponent(new MainGameOrderBtnDisplay())
 				.addComponent(new PauseMenuActions(this.game))
 				.addComponent(new PauseMenuDisplay(this))
-		        .addComponent(new EndDayDisplay())
-				.addComponent(new MoralDisplayTemp(this));
+		        .addComponent(new EndDayDisplay());
 
 		//temporary moral display
 //			.addComponent(new MoralDisplayTemp(this));
