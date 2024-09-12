fe09f942968bd2918ea6091a5467695da09691f1
Joanna Wang <s4740105@student.uq.edu.au>
2024-09-12 13:05:03 +1000

    add pause menu and moral display to maingamescreen

--- source/core/src/main/com/csse3200/game/screens/MainGameScreen.java
+++ source/core/src/main/com/csse3200/game/screens/MainGameScreen.java
@@ -207,7 +207,8 @@ public class MainGameScreen extends ScreenAdapter {
 			.addComponent(new MainGameOrderBtnDisplay())
 				.addComponent(new PauseMenuActions(this.game))
 				.addComponent(new PauseMenuDisplay(this))
-		        .addComponent(new EndDayDisplay());
+							.addComponent(new MoralDisplayTemp(this))
+				.addComponent(new EndDayDisplay());
 		//temporary moral display
 //			.addComponent(new MoralDisplayTemp(this));
 		ServiceLocator.getEntityService().register(ui);
