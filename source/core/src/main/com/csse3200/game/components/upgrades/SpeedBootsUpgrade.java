f41ed4631a8e5e2da67832a4d693f9a1c6198a83
Joanna Wang <s4740105@student.uq.edu.au>
2024-09-20 18:26:45 +1000

    handling timer so the speed boot can only be used for 1 minute

--- source/core/src/main/com/csse3200/game/components/upgrades/SpeedBootsUpgrade.java
+++ source/core/src/main/com/csse3200/game/components/upgrades/SpeedBootsUpgrade.java
@@ -4,24 +4,52 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.csse3200.game.components.CombatStatsComponent;
 import com.csse3200.game.components.Component;
+import com.csse3200.game.components.player.KeyboardPlayerInputComponent;
 import com.csse3200.game.entities.Entity;
+import com.csse3200.game.services.GameTime;
 import com.csse3200.game.services.ServiceLocator;
 
 public class SpeedBootsUpgrade extends Component {
 
+    private static final long BOOST_DURATION = 60000;
+    private static final float NORMAL_SPEED = 1f;
+    private static final float BOOSTED_SPEED = 2f;
     private CombatStatsComponent combatStatsComponent;
+    private KeyboardPlayerInputComponent keyboardPlayerInputComponent;
+    private final GameTime gameTime;
+    private long boostStartTime = -1;
 
     public SpeedBootsUpgrade() {
         super();
         ServiceLocator.getPlayerService().getEvents().addListener("playerCreated", (Entity player) -> {
             this.combatStatsComponent = player.getComponent(CombatStatsComponent.class);
+            this.keyboardPlayerInputComponent = player.getComponent(KeyboardPlayerInputComponent.class);
         });
+        gameTime = ServiceLocator.getTimeSource();
     }
+
     @Override
     public void update() {
-        if (Gdx.input.isKeyJustPressed(Input.Keys.B)) {
-            speedCost();
+        if (keyboardPlayerInputComponent != null) {
+            long current = gameTime.getTime();
+            if (Gdx.input.isKeyJustPressed(Input.Keys.B) && boostStartTime == -1) {
+                activate();
+            }
+            if (boostStartTime != -1 && current - boostStartTime > BOOST_DURATION) {
+                deactivate();
+            }
         }
+    }
+
+    private void activate() {
+        keyboardPlayerInputComponent.setWalkSpeed(BOOSTED_SPEED);
+        boostStartTime = gameTime.getTime();
+        speedCost();
+    }
+
+    private void deactivate() {
+        keyboardPlayerInputComponent.setWalkSpeed(NORMAL_SPEED);
+        boostStartTime = -1;
     }
 
     public void speedCost() {
