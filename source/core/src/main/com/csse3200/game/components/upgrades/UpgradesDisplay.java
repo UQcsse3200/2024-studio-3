0e0dcaea13b84b6220d0dc5b1658f266de65ae00
daleelakhairil <d.khairil@uq.edu.au>
2024-09-26 03:39:47 +1000

    add button

--- source/core/src/main/com/csse3200/game/components/upgrades/UpgradesDisplay.java
+++ source/core/src/main/com/csse3200/game/components/upgrades/UpgradesDisplay.java
@@ -7,7 +7,10 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.csse3200.game.components.maingame.PauseMenuDisplay;
 import com.csse3200.game.screens.MainGameScreen;
 import com.csse3200.game.services.ServiceLocator;
@@ -15,6 +18,8 @@ import com.csse3200.game.ui.UIComponent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,6 +33,8 @@ public class UpgradesDisplay extends UIComponent {
     private boolean isVisible = false;
     private List<Image> upgradeImages; // this is to store all the upgrades images
     private Table upgradesTable;
+
+    private Skin skin;
     private static final String[] upgradeTexturePaths = {
             "images/Speed_boot.png",
             "images/Rage.png",
@@ -75,17 +82,18 @@ public class UpgradesDisplay extends UIComponent {
         upgradesMenuImage = createUpgradesMenuDisplay();
         stage.addActor(upgradesMenuImage);
 
-        // Create a table for the upgrade images
         upgradesTable = new Table();
         upgradesTable.setFillParent(false);  // if set to true it will take the whole screem
         upgradesTable.setSize(400, 300);
         upgradesTable.setPosition(
                 upgradesMenuImage.getX() + 100,
-                upgradesMenuImage.getY() + 40
+                upgradesMenuImage.getY() + 30
         );
 
         upgradesTable.top().left();
         stage.addActor(upgradesTable);
+
+
 
         stage.addListener(new InputListener() {
             @Override
@@ -121,7 +129,37 @@ public class UpgradesDisplay extends UIComponent {
         return upgradeImage;
     }
 
+    private Table createButtonsTable() {
+        Table buttonTable = new Table();
 
+        // Create YES and NO buttons
+        TextButton yesButton = new TextButton("YES", skin);
+        TextButton noButton = new TextButton("NO", skin);
+
+        // Add click listeners for the buttons
+        yesButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                logger.info("YES button clicked");
+                // Handle YES button click
+            }
+        });
+
+        noButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                logger.info("NO button clicked");
+                // Handle NO button click
+            }
+        });
+
+        // Add buttons to the table with padding
+        buttonTable.add(yesButton).minWidth(200).minHeight(50).pad(10);
+        buttonTable.row();  // Move to the next row
+        buttonTable.add(noButton).minWidth(200).minHeight(50).pad(10);
+
+        return buttonTable;
+    }
     /**
      * Toggles the visibility of the upgrades menu
      */
