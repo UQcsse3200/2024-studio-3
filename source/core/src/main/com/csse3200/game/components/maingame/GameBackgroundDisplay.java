9eb528acf9bec2b09d152d6b515a6ad535680f9e
Alex Macnamara <alexmacnamara2004@gmail.com>
2024-09-27 19:06:53 +1000

    Overrode update in GameBackgroundDisplay to update the background to match the day cycle based off the 5 minute timer. Currently using stand in images that covers the game area (not fully functional). Added background images into MainGameScreen

--- source/core/src/main/com/csse3200/game/components/maingame/GameBackgroundDisplay.java
+++ source/core/src/main/com/csse3200/game/components/maingame/GameBackgroundDisplay.java
@@ -7,122 +7,26 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.TimeUtils;
+import com.csse3200.game.GdxGame;
 import com.csse3200.game.screens.MainGameScreen;
 import com.csse3200.game.services.ServiceLocator;
 import com.csse3200.game.ui.UIComponent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-//public class GameBackgroundDisplay extends UIComponent {
-//    private final MainGameScreen game;
-//    private Table table;
-//    private static final Logger logger = LoggerFactory.getLogger(PauseMenuDisplay.class);
-//    private static final String[] backgroundTextures = {"images/background_images/1.0.png",
-//            "images/background_images/1.5.png",
-//            "images/background_images/2.0.png",
-//            "images/background_images/2.5.png",
-//            "images/background_images/3.0.png",
-//            "images/background_images/3.5.png",
-//            "images/background_images/4.0.png",
-//            "images/background_images/4.5.png",
-//            "images/background_images/5.0.png",
-//            "images/background_images/5.5.png",
-//            "images/background_images/6.0.png",
-//            "images/background_images/6.5.png",
-//            "images/background_images/7.0.png",
-//            "images/background_images/7.5.png",
-//            "images/background_images/8.0.png",
-//            "images/background_images/8.5.png",
-//            "images/background_images/9.0.png",
-//            "images/background_images/9.5.png",
-//            "images/background_images/10.0.png",
-//            "images/background_images/10.5.png",
-//            "images/background_images/11.0.png",
-//            "images/background_images/11.5.png",
-//            "images/background_images/12.0.png",
-//            "images/background_images/12.5.png",
-//            "images/background_images/13.0.png",
-//            "images/background_images/13.5.png",
-//            "images/background_images/14.0.png",
-//            "images/background_images/14.5.png",
-//            "images/background_images/15.0.png",
-//            "images/background_images/15.5.png",
-//            "images/background_images/16.0.png",
-//            "images/background_images/16.5.png",
-//            "images/background_images/17.0.png",
-//            "images/background_images/17.5.png",
-//            "images/background_images/18.0.png",
-//            "images/background_images/18.5.png"};
-//
-//    private float screenWidth = Gdx.graphics.getWidth();
-//    private float screenHeight = Gdx.graphics.getHeight();
-//
-//
-//    public GameBackgroundDisplay(MainGameScreen game) {
-//        super();
-//        this.game = game;
-//    }
-//
-//    /**
-//     * Create the pause menu background image
-//     * @return backgroundImage
-//     */
-//    private Image createBackgroundImage() {
-//        Texture gameBackgroundTexture = ServiceLocator
-//                .getResourceService().getAsset("images/background_images/1.0.png", Texture.class);
-//
-//        Image backgroundImage = new Image(gameBackgroundTexture);
-//
-//        return backgroundImage;
-//
-//    }
-//
-//    private void createGameBackground() {
-//        ServiceLocator.getResourceService().loadTextures(backgroundTextures);
-//        table = new Table();
-//        table.setFillParent(true);
-//        Image backgroundImage = createBackgroundImage();
-//
-//        table.add(backgroundImage);
-//        table.setVisible(true);
-//
-//        stage.addActor(table);
-//    }
-//
-//    public void addActors() {
-//        createGameBackground();
-//    }
-//
-//
-//    public void create() {
-//        super.create();
-//        ServiceLocator.getResourceService().loadTextures(backgroundTextures);
-//        table = new Table();
-//        table.setFillParent(true);
-//        table.setVisible(true);
-//        createGameBackground();
-//        stage.addActor(table);
-//    }
-//
-//    @Override
-//    public void dispose() {
-//        super.dispose();
-//        ServiceLocator.getResourceService().unloadAssets(backgroundTextures);
-//    }
-//
-//    @Override
-//    protected void draw(SpriteBatch batch) {
-//    }
-//
-//    @Override
-//    public void setStage(Stage mock) {
-//    }
-//}
-
 public class GameBackgroundDisplay extends UIComponent {
     private Table table;
     private static final Logger logger = LoggerFactory.getLogger(PauseMenuDisplay.class);
-    private static final String[] backgroundTextures = {"images/background_images/1.0.png",
+    private float screenWidth = Gdx.app.getGraphics().getWidth();
+    private float screenHeight = Gdx.app.getGraphics().getHeight();
+    private float timePerFrame = 300/36; //seconds spent on each frame (assuming
+    // 5min day)
+    private boolean lastFrame;
+    private long timeSinceLastUpdate;
+    private String currentImage;
+    private int currentImageIndex;
+    private static final String[] BACKGROUNDTEXTURES = {"images/background_images/1.0.png",
             "images/background_images/1.5.png",
             "images/background_images/2.0.png",
             "images/background_images/2.5.png",
@@ -160,11 +64,15 @@ public class GameBackgroundDisplay extends UIComponent {
             "images/background_images/18.5.png",
             "images/b2.png"};
 
-    private float screenWidth = Gdx.graphics.getWidth();
-    private float screenHeight = Gdx.graphics.getHeight();
+
+
 
     public GameBackgroundDisplay() {
         super();
+        this.lastFrame = false;
+        this.currentImage = "images/background_images/1.0.png";
+        this.currentImageIndex = 0;
+        this.timeSinceLastUpdate = TimeUtils.millis();
     }
 
     @Override
@@ -175,11 +83,11 @@ public class GameBackgroundDisplay extends UIComponent {
         table.setVisible(true);
         stage.addActor(table);
 
-        setupBackground();
+        setBackground();
     }
 
-    private void setupBackground() {
-        Texture texture = ServiceLocator.getResourceService().getAsset("images/b2.png",
+    private void setBackground() {
+        Texture texture = ServiceLocator.getResourceService().getAsset(currentImage,
                 Texture.class);
         Image image = new Image(texture);
         image.setFillParent(true);
@@ -193,4 +101,24 @@ public class GameBackgroundDisplay extends UIComponent {
     @Override
     public void setStage(Stage mock) {
     }
+
+    @Override
+    public void update() {
+        if (!lastFrame) {
+            long elapsedTime = TimeUtils.timeSinceMillis(timeSinceLastUpdate);
+            long elapsedTimeSecs = elapsedTime/1000;
+            if (elapsedTimeSecs >= this.timePerFrame) {
+                this.currentImageIndex++;
+                this.currentImage = BACKGROUNDTEXTURES[currentImageIndex];
+                this.timeSinceLastUpdate = TimeUtils.millis();
+                table.clear();
+                setBackground();
+                System.out.println("Updated background to " + this.currentImage);
+                if (currentImageIndex >= 35) {
+                    this.lastFrame = true; //stops updating when hits last frame of cycle
+                }
+            }
+        }
+    }
+
 }
