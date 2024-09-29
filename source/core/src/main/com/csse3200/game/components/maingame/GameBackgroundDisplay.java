c62e8704f95feec44755ccf48f0220a322f7aefb
Alex Macnamara <alexmacnamara2004@gmail.com>
2024-09-29 21:39:25 +1000

    Added final background assets and uncommented update function in GameBackgroundDisplay. Game background now loads and cycles through as intended (formal testing still needed)

--- source/core/src/main/com/csse3200/game/components/maingame/GameBackgroundDisplay.java
+++ source/core/src/main/com/csse3200/game/components/maingame/GameBackgroundDisplay.java
@@ -61,8 +61,7 @@ public class GameBackgroundDisplay extends UIComponent {
             "images/background_images/17.0.png",
             "images/background_images/17.5.png",
             "images/background_images/18.0.png",
-            "images/background_images/18.5.png",
-            "images/b2.png"};
+            "images/background_images/18.5.png"};
 
 
 
@@ -70,7 +69,7 @@ public class GameBackgroundDisplay extends UIComponent {
     public GameBackgroundDisplay() {
         super();
         this.lastFrame = false;
-        this.currentImage = "images/b2.png";
+        this.currentImage = "images/background_images/1.0.png";
         this.currentImageIndex = 0;
         this.timeSinceLastUpdate = TimeUtils.millis();
     }
@@ -102,23 +101,23 @@ public class GameBackgroundDisplay extends UIComponent {
     public void setStage(Stage mock) {
     }
 
-//    @Override
-//    public void update() {
-//        if (!lastFrame) {
-//            long elapsedTime = TimeUtils.timeSinceMillis(timeSinceLastUpdate);
-//            long elapsedTimeSecs = elapsedTime/1000;
-//            if (elapsedTimeSecs >= this.timePerFrame) {
-//                this.currentImageIndex++;
-//                this.currentImage = BACKGROUNDTEXTURES[currentImageIndex];
-//                this.timeSinceLastUpdate = TimeUtils.millis();
-//                table.clear();
-//                setBackground();
-//                System.out.println("Updated background to " + this.currentImage);
-//                if (currentImageIndex >= 35) {
-//                    this.lastFrame = true; //stops updating when hits last frame of cycle
-//                }
-//            }
-//        }
-//    }
+    @Override
+    public void update() {
+        if (!lastFrame) {
+            long elapsedTime = TimeUtils.timeSinceMillis(timeSinceLastUpdate);
+            long elapsedTimeSecs = elapsedTime/1000;
+            if (elapsedTimeSecs >= this.timePerFrame) {
+                this.currentImageIndex++;
+                this.currentImage = BACKGROUNDTEXTURES[currentImageIndex];
+                this.timeSinceLastUpdate = TimeUtils.millis();
+                table.clear();
+                setBackground();
+                System.out.println("Updated background to " + this.currentImage);
+                if (currentImageIndex >= 35) {
+                    this.lastFrame = true; //stops updating when hits last frame of cycle
+                }
+            }
+        }
+    }
 
 }
