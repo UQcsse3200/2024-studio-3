fc9fb52785b9e05d21d5ae2dccd4be1a300e7327
Callan <callanvragalis@gmail.com>
2024-10-01 00:56:22 +1000

    Removed ability for other stations to use q to ineract normal (and made e only)

--- source/core/src/main/com/csse3200/game/components/station/StationItemHandlerComponent.java
+++ source/core/src/main/com/csse3200/game/components/station/StationItemHandlerComponent.java
@@ -2,15 +2,12 @@ package com.csse3200.game.components.station;
 
 import java.util.ArrayList;
 
-import com.badlogic.gdx.net.Socket;
 import com.csse3200.game.components.Component;
-import com.csse3200.game.components.items.ChopIngredientComponent;
 import com.csse3200.game.components.items.IngredientComponent;
 import com.csse3200.game.components.items.ItemComponent;
 import com.csse3200.game.components.player.InventoryComponent;
 import com.csse3200.game.components.player.InventoryDisplay;
 import com.csse3200.game.components.station.loader.StationAcceptableItemsGetter;
-import com.csse3200.game.entities.Entity;
 
 public class StationItemHandlerComponent extends Component {
     /**
