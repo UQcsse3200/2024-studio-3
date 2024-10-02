4c9e7d764751a2cab3aab5815f919b40cb9b7c7c
Danh Dinh <danhdinh98@gmail.com>
2024-10-02 19:26:08 +1000

    Added all map stages, implemented correct stations for stage 1 and 2

--- source/core/src/main/com/csse3200/game/services/MapLayout.java
+++ source/core/src/main/com/csse3200/game/services/MapLayout.java
@@ -18,8 +18,8 @@ import java.util.Objects;
 
 public class MapLayout extends GameArea{
     private EventHandler mapEventHandler;
-    private static final String mapLevel1 = "images/map/map_test.txt";
-    //private static final String mapLevel1 = "images/map/map_two.txt";
+    //private static final String mapLevel1 = "images/map/map_test.txt";
+    private static final String mapLevel1 = "images/map/map_base.txt";
     private int strToNum;
     private int strToNum2;
     private ArrayList<Bench> benches = new ArrayList<Bench>();
