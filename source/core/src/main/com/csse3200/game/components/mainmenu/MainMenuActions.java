3d48914aa9a85aae6a19fb8d2cd1e1b13b47c382
abhat0 <58474871+abhat0@users.noreply.github.com>
2024-10-06 22:27:29 +1000

    #474 remove use of enter key for displaying pause menu

--- source/core/src/main/com/csse3200/game/components/mainmenu/MainMenuActions.java
+++ source/core/src/main/com/csse3200/game/components/mainmenu/MainMenuActions.java
@@ -50,7 +50,6 @@ public class MainMenuActions extends Component {
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy_HH-mm-ss");
     String formattedDateTime = currentDateTime.format(formatter);
     ServiceLocator.getSaveLoadService().setSaveFile(formattedDateTime + ".json");
-    //game.setScreen(GdxGame.ScreenType.MAIN_GAME);
   }
 
   /**
