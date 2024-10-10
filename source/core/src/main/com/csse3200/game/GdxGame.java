beb299b1a4690fc0e83a8c796e7a8bef585fb495
ashom2 <alekid804@gmail.com>
2024-10-10 13:37:51 +1000

    added getter method for current game screen type

--- source/core/src/main/com/csse3200/game/GdxGame.java
+++ source/core/src/main/com/csse3200/game/GdxGame.java
@@ -26,6 +26,7 @@ import static com.badlogic.gdx.Gdx.app;
 public class GdxGame extends Game {
   private static final Logger logger = LoggerFactory.getLogger(GdxGame.class);
   private Screen previousScreen;
+  private ScreenType currentScreenType;
   private Texture backgroundTexture;
   private SpriteBatch batch;
   private Cutscene currentCutscene;
@@ -60,6 +61,7 @@ public class GdxGame extends Game {
   public void setScreen(ScreenType screenType) {
     logger.info("Setting game screen to {}", screenType);
     Screen currentScreen = getScreen();
+    currentScreenType = screenType;
 
     previousScreen = currentScreen;  // Save the current screen before changing
     SaveLoadService system = ServiceLocator.getSaveLoadService();
@@ -79,6 +81,14 @@ public class GdxGame extends Game {
    */
   public Screen getPreviousScreen() {
     return previousScreen;
+  }
+
+  /**
+   * Get the previous game's screen
+   * @return previous screen
+   */
+  public ScreenType getCurrentScreenType() {
+    return currentScreenType;
   }
 
   @Override
