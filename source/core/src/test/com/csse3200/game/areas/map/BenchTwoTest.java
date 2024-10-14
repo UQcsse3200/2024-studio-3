f5f5f40a2321f1446baad2784768bac86f28562c
Alex Macnamara <alexmacnamara2004@gmail.com>
2024-10-14 12:00:25 +1000

    Added teardown script in BenchTwoTest

--- source/core/src/test/com/csse3200/game/areas/map/BenchTwoTest.java
+++ source/core/src/test/com/csse3200/game/areas/map/BenchTwoTest.java
@@ -8,6 +8,7 @@ import com.csse3200.game.extensions.GameExtension;
 import com.csse3200.game.rendering.RenderService;
 import com.csse3200.game.services.ResourceService;
 import com.csse3200.game.services.ServiceLocator;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -23,7 +24,6 @@ import static org.mockito.Mockito.*;
 @ExtendWith(GameExtension.class)
 public class BenchTwoTest {
     private AutoCloseable mocks;
-    private Entity entity;
     private BenchGenerator benchGenerator;
 
     @BeforeEach
@@ -38,6 +38,12 @@ public class BenchTwoTest {
         ServiceLocator.registerResourceService(resourceService);
 
         benchGenerator = spy(new BenchGenerator());
+    }
+
+    @AfterEach
+    void end() throws Exception{
+        mocks.close();
+        ServiceLocator.clear();
     }
 
     @Test
