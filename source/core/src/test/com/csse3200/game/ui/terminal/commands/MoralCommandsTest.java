9a146700907b06b7961acd96838036dc64f7c12a
manpreetsingh <yoyomanpreetsingh2014@gmail.com>
2024-10-15 00:20:05 +1000

    Fixed minor code smells in MoralCommands.java and its tests

--- source/core/src/test/com/csse3200/game/ui/terminal/commands/MoralCommandsTest.java
+++ source/core/src/test/com/csse3200/game/ui/terminal/commands/MoralCommandsTest.java
@@ -7,7 +7,6 @@ import com.csse3200.game.entities.Entity;
 import com.csse3200.game.extensions.GameExtension;
 import com.csse3200.game.services.ServiceLocator;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.util.ArrayList;
