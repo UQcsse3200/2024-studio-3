329610b3c19922f190a7088ade2fad1766ffc33e
NIMESH GARG <51132740+nimeshgarg@users.noreply.github.com>
2024-10-17 04:26:17 +1000

    Moral Day 1 and 4 Code Smell

--- source/core/src/main/com/csse3200/game/components/cutscenes/MoralDay1Cutscene.java
+++ source/core/src/main/com/csse3200/game/components/cutscenes/MoralDay1Cutscene.java
@@ -30,17 +30,15 @@ public class MoralDay1Cutscene extends Cutscene {
         sceneText.add("Mafia Boss > Because I'm sooo generous I'll even let you keep 40 coins.");
         sceneText.add("Mafia Boss > What do you say?");
 
-        String mafiaImage = MAFIA_IMAGE_PATH;
         Vector2 mafiaPosition = new Vector2(3, -1);
         float mafiaScale = 4.0f;
 
-        String iconImage = ICON_IMAGE_PATH;
         Vector2 iconPosition = new Vector2(-8, -2);
         float iconScale = 7.0f;
 
         Scene scene = new Scene(DAY_2_SCENE_PATH);
         scene.setImages(
-                new String[]{mafiaImage, iconImage},
+                new String[]{MAFIA_IMAGE_PATH, ICON_IMAGE_PATH},
                 new Vector2[]{mafiaPosition, iconPosition},
                 new float[]{mafiaScale, iconScale}
         );
@@ -55,11 +53,9 @@ public class MoralDay1Cutscene extends Cutscene {
         Array<String> sceneText = new Array<>();
         sceneText.add("Press 'y' for yes and 'n' for no");
 
-        String mafiaImage = MAFIA_IMAGE_PATH;
         Vector2 mafiaPosition = new Vector2(3, -1);
         float mafiaScale = 4.0f;
 
-        String iconImage = ICON_IMAGE_PATH;
         Vector2 iconPosition = new Vector2(-8, -2);
         float iconScale = 7.0f;
 
@@ -73,7 +69,7 @@ public class MoralDay1Cutscene extends Cutscene {
 
         Scene scene = new Scene(DAY_2_SCENE_PATH);
         scene.setImages(
-                new String[]{mafiaImage, iconImage, yesImage, noImage},
+                new String[]{MAFIA_IMAGE_PATH, ICON_IMAGE_PATH, yesImage, noImage},
                 new Vector2[]{mafiaPosition, iconPosition, yesPosition, noPosition},
                 new float[]{mafiaScale, iconScale, yesScale, noScale}
         );
