f255415c9a5aa7fd9a9ba6f3471af5e80b5c9b3f
Caleb Murphy <calebmurphy2003@gmail.com>
2024-10-17 01:28:11 +1000

    #502 Fixed code smells for ChopIngredientConfig.java

--- source/core/src/main/com/csse3200/game/entities/configs/ChopIngredientConfig.java
+++ source/core/src/main/com/csse3200/game/entities/configs/ChopIngredientConfig.java
@@ -6,7 +6,7 @@ import java.util.Map;
  * Defines the properties stored in ChopIngredient config files to be loaded by the Ingredient Factory.
  */
 public class ChopIngredientConfig {
-    public Map<String, Integer> ingredientChopTimes;
+    public static Map<String, Integer> ingredientChopTimes;
 
     public int getCookTime(String ingredientName) {
         return ingredientChopTimes.get(ingredientName);
