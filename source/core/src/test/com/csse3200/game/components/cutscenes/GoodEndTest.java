c925019b587f212709fbf3e009783ce3d03dc8ba
Caleb Murphy <calebmurphy2003@gmail.com>
2024-10-17 11:06:54 +1000

    #652 Fixed code smells for: GoodEndTest.java

--- source/core/src/test/com/csse3200/game/components/cutscenes/GoodEndTest.java
+++ source/core/src/test/com/csse3200/game/components/cutscenes/GoodEndTest.java
@@ -1,11 +1,6 @@
 package com.csse3200.game.components.cutscenes;
 
-import com.csse3200.game.GdxGame;
-import com.csse3200.game.areas.terrain.TerrainFactory;
-import com.csse3200.game.components.upgrades.UpgradesDisplay;
-import com.csse3200.game.entities.Entity;
 import com.csse3200.game.entities.EntityService;
-import com.csse3200.game.entities.factories.ItemFactory;
 import com.csse3200.game.services.ServiceLocator;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,24 +10,11 @@ import static org.mockito.Mockito.*;
 
 public class GoodEndTest {
 
-    private GoodEnd goodEnd;
-    private TerrainFactory terrainFactory;
-    private GdxGame.LevelType levelType;
-    private UpgradesDisplay upgradesDisplay;
-    private EntityService entityService;
-
     @Before
     public void setUp() {
-        terrainFactory = mock(TerrainFactory.class);
-        levelType = GdxGame.LevelType.LEVEL_1;  // Choose a suitable level type
-        upgradesDisplay = mock(UpgradesDisplay.class);
-
         // Register EntityService with ServiceLocator to track entity spawning
-        entityService = mock(EntityService.class);
+        EntityService entityService = mock(EntityService.class);
         ServiceLocator.registerEntityService(entityService);
-
-        // Initialize GoodEnd instance
-        goodEnd = new GoodEnd(terrainFactory, levelType, upgradesDisplay);
     }
 
     @Test
