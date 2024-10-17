f335872e106749551dc8f74a3e29d6852c5ddbbf
Serene McCormack <serenemccormack@gmail.com>
2024-10-17 14:04:40 +1000

    Update PlateComponent tests.

--- source/core/src/test/com/csse3200/game/components/items/PlateComponentTest.java
+++ source/core/src/test/com/csse3200/game/components/items/PlateComponentTest.java
@@ -3,6 +3,7 @@ package com.csse3200.game.components.items;
 import com.csse3200.game.entities.Entity;
 import org.junit.jupiter.api.BeforeEach;
 import static org.mockito.Mockito.*;
+import org.junit.jupiter.api.Test;
 
 class PlateComponentTest {
 
@@ -19,6 +20,47 @@ class PlateComponentTest {
         plateComponent = new PlateComponent(3);
         plateComponent.setEntity(mockEntity);
 
+    }
+
+    @Test
+    void cleanOnCreationState()  {
+        assert plateComponent.getPlateState() == PlateComponent.PlateState.CLEAN;
+    }
+
+    @Test
+    void cleanOnCreationGetter() {
+        assert plateComponent.isClean() == true;
+    }
+
+    @Test
+    void idOnCreation() {
+        assert plateComponent.getPlateId() == 3;
+    }
+
+    @Test
+    void usePlateState() {
+        plateComponent.usePlate();
+        assert plateComponent.getPlateState() == PlateComponent.PlateState.DIRTY;
+    }
+
+    @Test
+    void usePlateGetter() {
+        plateComponent.usePlate();
+        assert plateComponent.isClean() == false;
+    }
+
+    @Test
+    void useThenCleanState() {
+        plateComponent.usePlate();
+        plateComponent.washPlate();
+        assert plateComponent.getPlateState() == PlateComponent.PlateState.CLEAN;
+    }
+
+    @Test
+    void useThenCleanGetter() {
+        plateComponent.usePlate();
+        plateComponent.washPlate();
+        assert plateComponent.isClean() == true;
     }
 
 }
