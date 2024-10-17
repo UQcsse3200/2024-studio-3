303d6ca6fd13c09e6ddb5b6053da5b3e67e43b66
Aiden Richards <aiden.richards@student.uq.edu.au>
2024-10-17 12:15:52 +1000

    Unit tests for Moral TextDisplay components

--- source/core/src/test/com/csse3200/game/components/maingame/TestTextDisplayMoral.java
+++ source/core/src/test/com/csse3200/game/components/maingame/TestTextDisplayMoral.java
@@ -1,14 +1,9 @@
 package com.csse3200.game.components.maingame;
 
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.csse3200.game.extensions.GameExtension;
-import com.csse3200.game.screens.CutsceneScreen;
 import com.csse3200.game.services.GameTime;
 import com.csse3200.game.services.ServiceLocator;
 import com.csse3200.game.components.cutscenes.Cutscene;
@@ -17,15 +12,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.MockedStatic;
-import org.mockito.junit.jupiter.MockitoExtension;
 import com.csse3200.game.services.DayNightService;
-
-import java.util.Arrays;
-
-import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.*;
 @ExtendWith(GameExtension.class)
 public class TestTextDisplayMoral {
 
