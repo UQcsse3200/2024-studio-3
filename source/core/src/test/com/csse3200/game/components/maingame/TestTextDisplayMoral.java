0445441beaaa80a6173b75399a7ac0a90f3b9b12
Aiden Richards <aiden.richards@student.uq.edu.au>
2024-10-17 12:16:39 +1000

    Unit tests for Moral TextDisplay components

--- source/core/src/test/com/csse3200/game/components/maingame/TestTextDisplayMoral.java
+++ source/core/src/test/com/csse3200/game/components/maingame/TestTextDisplayMoral.java
@@ -1,7 +1,6 @@
 package com.csse3200.game.components.maingame;
 
 import com.badlogic.gdx.ScreenAdapter;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.csse3200.game.extensions.GameExtension;
 import com.csse3200.game.services.GameTime;
@@ -12,7 +11,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import com.csse3200.game.services.DayNightService;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 @ExtendWith(GameExtension.class)
 public class TestTextDisplayMoral {
